ed96b61f2e47c88920a06ba706966d4653e8d9d9
BenDoschGit <benjamindosch@gmail.com>
2021-05-31 11:43:52 -0500

    Starting point

---
+++ app/src/main/java/org/brokenarrowmuseum/scavenger_hunt_and_trivia/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.brokenarrowmuseum.scavenger_hunt_and_trivia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
