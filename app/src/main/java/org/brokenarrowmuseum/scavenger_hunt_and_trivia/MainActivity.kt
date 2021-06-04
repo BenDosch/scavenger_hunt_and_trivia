e57c89e4a6f3ded7c47606f0f0dfec5c78cf4d90
blanketmanatee <ashleyl.welch@gmail.com>
2021-06-04 12:56:21 -0500

    Recyclerview but not done yet

--- app/src/main/java/org/brokenarrowmuseum/scavenger_hunt_and_trivia/MainActivity.kt
+++ app/src/main/java/org/brokenarrowmuseum/scavenger_hunt_and_trivia/MainActivity.kt
@@ -3,9 +3,14 @@ package org.brokenarrowmuseum.scavenger_hunt_and_trivia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+private lateinit var linearLayoutManager: LinearLayoutManager
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = linearLayoutManager
+
     }
 }
