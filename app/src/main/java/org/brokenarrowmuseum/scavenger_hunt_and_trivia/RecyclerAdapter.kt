0511ea3ee7fc12ec2b3360d4abaa2f5fbad63484
blanketmanatee <ashleyl.welch@gmail.com>
2021-06-05 16:44:10 -0500

    trying to fix

--- app/src/main/java/org/brokenarrowmuseum/scavenger_hunt_and_trivia/RecyclerAdapter.kt
+++ app/src/main/java/org/brokenarrowmuseum/scavenger_hunt_and_trivia/RecyclerAdapter.kt
@@ -2,18 +2,3 @@ package org.brokenarrowmuseum.scavenger_hunt_and_trivia
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-
-class RecyclerAdapter(private val photos: ArrayList<Photo>) : RecyclerView.Adapter<RecyclerAdapter.PhotoHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.PhotoHolder {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: RecyclerAdapter.PhotoHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount() = photos.size
-
-}
-}
