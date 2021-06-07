389ef128816ea8a57c00fde4cdd1bff4d41a8573
blanketmanatee <ashleyl.welch@gmail.com>
2021-06-07 17:18:34 -0500

    implemented recyclerview and flexbox for scavenger hunt

--- app/src/main/java/org/brokenarrowmuseum/scavenger_hunt_and_trivia/MainActivity.kt
+++ app/src/main/java/org/brokenarrowmuseum/scavenger_hunt_and_trivia/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
 import org.brokenarrowmuseum.scavenger_hunt_and_trivia.data.entities.Question
 import org.brokenarrowmuseum.scavenger_hunt_and_trivia.data.storage.Database
 
@@ -13,11 +17,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        val layoutManager = FlexboxLayoutManager(this)
+        layoutManager.flexDirection = FlexDirection.ROW
+        layoutManager.justifyContent = JustifyContent.CENTER
+        recyclerView.layoutManager = layoutManager
+
+        val divLikeContent = ArrayList<String>()
+        /*divLikeContent.add("Route")
+        divLikeContent.add("No calls during the ride")
+        divLikeContent.add("Smell")
+        divLikeContent.add("Less talk")
+        divLikeContent.add("Safety")*/
+
+        val adapter = DivLikeAdapter(divLikeContent)
+
+        recyclerView.adapter = adapter
+
         val btnEdit = findViewById<Button>(R.id.btnEdit)
         btnEdit.setOnClickListener {
             val intent = Intent(this, Admin::class.java)
             startActivity(intent)
         }
 
+    }
+    private fun generateRandonText(): String {
+        val sb = StringBuilder()
+        for (i in 1 + (Math.random() * 20).toInt() downTo 1) {
+            sb.append("a")
+        }
+        return sb.toString()
     }
 }
